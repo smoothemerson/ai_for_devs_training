f34b9bad291b7567c0f988e9ef85bc4b82cb55f6
Emerson <emerson.faria@serpro.gov.br>
2025-11-19 07:50:25 -0300

    feat: add finishing dataset

--- classes/optimizing_llms_with_fine_tuning/dataset-preparation.py
+++ classes/optimizing_llms_with_fine_tuning/dataset-preparation.py
@@ -27,4 +27,57 @@ datasets = datasets.remove_columns(
 
 # %%
 datasets = datasets.train_test_split(test_size=0.2)
+
+
 # %%
+def removeN(example):
+    example["text"] = example["text"].replace("\n", "")
+    return example
+
+
+# %%
+datasets = datasets.map(removeN)
+
+
+# %%
+# label 0 -> No Hate Speech
+# label 1 -> Hate Speech
+def labelChange(example):
+    example["label_text"] = "No Hate Speech" if example["label"] == 0 else "Hate Speech"
+    return example
+
+
+# %%
+datasets = datasets.map(labelChange)
+
+# %%
+datasets = datasets.remove_columns(["label"])
+
+# %%
+print(datasets)
+
+
+# %%
+# Construção do objeto para openai
+def dataset_to_jsonl(dataset, file_name):
+    with open(file_name, "w", encoding="utf-8") as f:
+        for example in dataset:
+            json_obj = {
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": "Seu trabalho é classificar os comentários dos usuários em Hate Speech ou No Hate Speech.",
+                    },
+                    {"role": "user", "content": example["text"]},
+                    {"role": "assistant", "content": example["label_text"]},
+                ]
+            }
+
+            f.write(json.dumps(json_obj, ensure_ascii=False) + "\n")
+
+
+# %%
+dataset_to_jsonl(datasets["train"], "train.jsonl")
+
+# %%
+dataset_to_jsonl(datasets["test"], "validation.jsonl")

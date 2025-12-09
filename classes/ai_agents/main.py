09a4692d97058625020a58b30684bd09cffefb57
Emerson <emerson.faria@serpro.gov.br>
2025-12-09 12:59:44 -0300

    feat: add stock price analystic agent

--- classes/ai_agents/main.py
+++ classes/ai_agents/main.py
@@ -1,7 +1,10 @@
 # %%
 import os
+from datetime import datetime, timedelta
 
+import yfinance as yf
 from crewai import Agent, Crew, Process, Task
+from crewai.tools import tool
 from crewai_tools import CSVSearchTool
 from langchain_ollama import ChatOllama
 
@@ -55,3 +58,45 @@ getCustomerWallet = Task(
     expected_output="If the customer owns the stock, provide the mean price paid and the total stock numbers.",
     agent=customerManager,
 )
+
+
+# %%
+@tool(
+    "Yahoo Finance Tool",
+)
+def fetch_stock_price(ticket: str):
+    """Fetches stocks prices for {ticket} from the last year about a specific company from the Yahoo Finance API."""
+    end_date = datetime.today()
+    start_date = end_date - timedelta(days=365)
+    stock = yf.download(
+        ticket, start=start_date.strftime("%Y-%m-%d"), end=end_date.strftime("%Y-%m-%d")
+    )
+    return stock
+
+
+# %%
+stocketPriceAnalyst = Agent(
+    role="Senior Stock Price Analyst",
+    goal="Find the {ticket} stock price and analyses price trends. Compare with the price that the customer paid.",
+    backstory="""
+    You're a highly experienced in analyzing the price of specific stocks and make predictions about its future price.
+    """,
+    verbose=True,
+    llm=llm,
+    max_iter=5,
+    allow_delegation=False,
+    memory=True,
+)
+
+# %%
+getStockPrice = Task(
+    description="Analyze the stock {ticket} price history and create a price trend analyses of up, down or sideways.",
+    expected_output="""
+  Specify the current trend stocks price - Up, down or sideways.
+  eg. 'stock=AAPL, price UP'
+  """,
+    tools=[fetch_stock_price],
+    agent=stocketPriceAnalyst,
+)
+
+# %%
